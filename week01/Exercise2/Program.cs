1ea63138a73cb0c3d865e6ec1a9bfc085b9a8559
renanlabado <rlabado@byupathway.edu>
2026-06-29 23:50:17 +0800

    error in committing was removed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,17 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
-
-        int x = 5;
-        int y = 2; 
-
-        if (x > y)
-        {
-            Console.WriteLine("Greater");
-        }
-        else
-        {
-            Console.WriteLine("Not Greater");
-        }
     }
 }
