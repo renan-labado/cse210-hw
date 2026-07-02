bc6d4f8c78aec6ea64ad24da30eb48e42c4a3723
renanlabado <rlabado@byupathway.edu>
2026-07-02 13:14:47 +0800

    Finished Exercises3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+        //Console.Write("What is the magic number? ");
+        //string magicNumberText = Console.ReadLine();
+        //int magicNumber = int.Parse(magicNumberText);
+        int guessNumber = 0;
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int numberOfGuesses = 0;
+        Console.WriteLine(magicNumber);
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string guessNumberText = Console.ReadLine();
+            guessNumber = int.Parse(guessNumberText);
+            numberOfGuesses += 1;
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Got it!");
+            }
+        }
+        Console.WriteLine($"You guessed {numberOfGuesses} times!");
+    }   
 }
