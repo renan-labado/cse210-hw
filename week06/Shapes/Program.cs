2be71e990a1a6fcd00eb31ab3186c3526b6a6b05
renanlabado <rlabado@byupathway.edu>
2026-08-04 02:57:54 -0600

    finished learning act for w6

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.Clear();
+        Shape square = new Square(4, "blue");
+
+        Shape rectangle = new Rectangle(5, 4, "black");
+
+        Shape circle = new Circle(2, "blue");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area} \n");
+        }
     }
 }
